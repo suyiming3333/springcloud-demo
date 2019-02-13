704b82db7f6ee658502fc25270434f790f67c6e2
suyiming3333 <suyiming3333@gmail.com>
2019-02-13 20:31:39 +0800

    zuul网关服务demo

--- member-service/src/main/java/com/sym/memberservice/controller/MemberController.java
+++ member-service/src/main/java/com/sym/memberservice/controller/MemberController.java
@@ -1,5 +1,6 @@
 package com.sym.memberservice.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,12 +10,16 @@ import java.util.List;
 @RestController
 public class MemberController {
 
+    @Value("${server.port}")
+    private String serverPort;
+
     @RequestMapping("/getMemberList")
     public List<String> getUserList() {
         List<String> listUser = new ArrayList<String>();
         listUser.add("zhangsan");
         listUser.add("lisi");
         listUser.add("yushengjun");
+        listUser.add("serverPort:"+serverPort);
         return listUser;
     }
 }
